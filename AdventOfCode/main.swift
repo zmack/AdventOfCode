44fc7e16cb4cf6121ec26f1e401ddb54775f11f0
Andrei Bocan <zmaxor@gmail.com>
2016-01-02 22:04:04 +0200

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Andrei Bocan on 02/01/16.
+//  Copyright © 2016 Andrei Bocan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
